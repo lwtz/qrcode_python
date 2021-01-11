7f309890c32329f23aaa767994186c29581b405d
will.i.am ~ William ~ <w.h@outlook.com.br>
2021-01-11 09:01:04 -0300

    Create png.py

---
+++ png.py
@@ -0,0 +1,14 @@
+!pip install qrcode
+import qrcode
+
+qr = qrcode.QRCode(version=1,
+                   error_correction=qrcode.constants.ERROR_CORRECT_L,
+                   box_size=50,
+                   border=1)
+
+qr.add_data('www.google.com')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black",
+                    back_color='white')
+img.save('name.png')
